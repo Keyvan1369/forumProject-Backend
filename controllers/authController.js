c2af8aea8d457bbffb8e8d9b860ecea07a32c748
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-08-10 21:28:20 +0200

    update authController-errorHnade

---
+++ controllers/authController.js
@@ -0,0 +1,41 @@
+const login = (req, res) => {
+  let { email, password } = req.body;                    
+
+  email = email.trim().toLowerCase();    
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!password) {
+    return res.status(400).send("password is required" );
+ 
+  } else if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+  res.status(200).send("signup Res2");
+};
+
+const signup = (req, res) => {
+  /*   console.log(req.query);
+  console.log(req.body.pass); */
+  let { name, email, password } = req.body;                    //change const to let
+
+  email = email.trim().toLowerCase();    
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!password) {
+    return res.status(400).send("password is required" );
+  } else if (!name) {
+    return res.status(400).json({ message: "name is required" });
+  } else if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  res.status(200).send("signup Res2");
+};
+
+const forgotPassword = (req, res) => {
+  res.send("forgotPassword Res");
+};
+
+module.exports = {
+  login,
+  signup,
+  forgotPassword,
+};
