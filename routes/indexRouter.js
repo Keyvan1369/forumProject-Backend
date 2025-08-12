d67b98f8385ef717249bbbb23cb646de2cb3811b
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-08-12 14:47:48 +0200

    update with post_method

---
+++ routes/indexRouter.js
@@ -0,0 +1,12 @@
+
+const express = require("express")
+const indexRouter= express.Router()
+
+const { index, NotFound } = require("../controllers/indexController");
+
+
+indexRouter.get("/" , index)
+indexRouter.get("/notFound" , NotFound) 
+
+
+module.exports= indexRouter 
