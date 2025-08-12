1bbbefca985a0b9aeab754a9bd11cdb035515315
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-08-12 15:14:27 +0200

    reform DB_connection

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const { json } = require('body-parser')
-const authRouter = require('./routes/authRouter')
-const indexRouter = require('./routes/indexRouter')
-const userRouter = require('./routes/userRouter')
-const { default: mongoose } = require('mongoose')
-const app = express()
-const port = 5010
+const express = require("express");
+const { json } = require("body-parser");
+const authRouter = require("./routes/authRouter");
+const indexRouter = require("./routes/indexRouter");
+const userRouter = require("./routes/userRouter");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+const app = express();
+const port = 5010;
 
-app.use(express.json())
-app.use(authRouter)
-app.use(indexRouter)
-app.use(userRouter)
-mongoose.connect("mongodb+srv://keyvan:yRCEqJAhNS8g0GxL@cluster0.0qpal2c.mongodb.net/Forum_project")
-.catch((erro)=>console.log("error db: "+erro))
+
+app.use(express.json());
+app.use(authRouter);
+app.use(indexRouter);
+app.use(userRouter);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to DB:", err));
+ 
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
