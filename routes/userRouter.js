891a39afd9ac27eaa4719ca42c3f3de22cb5fa7e
Keyvan Hojabr <keyvanhojabr@gmail.com>
2025-08-15 18:14:31 +0200

    make dynamic routers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,11 +5,11 @@ const userRouter= express.Router()
 const { getUser, getSingleUser, createUser, updateUser, deleteUser } = require("../controllers/userController");
 
 
-userRouter.get("/user" , getUser)
-userRouter.get("/user/oneUser" , getSingleUser)
-userRouter.post("/user" , createUser) 
-userRouter.put("/user" , updateUser) 
-userRouter.delete("/user" , deleteUser) 
+userRouter.get("/users" , getUser)
+userRouter.get("/users/:id" , getSingleUser)
+userRouter.post("/users" , createUser) 
+userRouter.put("/users/:id" , updateUser) 
+userRouter.delete("/users/:id" , deleteUser) 
 
 
 
